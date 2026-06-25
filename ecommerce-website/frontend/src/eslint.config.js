c69e0fbdb5b905b49db493adf887295422047c45
Omoit699 <lawrenceomoit66@gmai;.com>
2026-06-25 19:28:16 +0300

    Deleted and replaced eslint.config.js file

---
+++ ecommerce-website/frontend/src/eslint.config.js
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.{js,jsx}"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        fetch: "readonly",
+        localStorage: "readonly",
+        console: "readonly",
+        window: "readonly",
+        document: "readonly"
+      }
+    },
+
+    plugins: {
+      react,
+      "react-hooks": reactHooks
+    },
+
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "no-unused-vars": "warn"
+    }
+  }
+];
