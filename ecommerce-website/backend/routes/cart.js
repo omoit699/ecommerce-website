9fc1d4b706904ac6df6c3d94a19ae23a9dd57728
Omoit699 <lawrenceomoit66@gmai;.com>
2026-06-26 18:02:35 +0300

    FIX CART ROUTES

--- ecommerce-website/backend/routes/cart.js
+++ ecommerce-website/backend/routes/cart.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import CartController from "../controllers/cartController.js";
+import cartController from "../controllers/cartController.js";
 
 const router = Router();
-const cartController = new CartController();
 
-router.get("/:userId", cartController.getCart.bind(cartController));
-router.post("/:userId/add", cartController.addItem.bind(cartController));
-router.delete("/:userId/remove/:productId", cartController.removeItem.bind(cartController));
-router.put("/:userId/update/:productId", cartController.updateItemQuantity.bind(cartController));
+router.get("/", cartController.getCart.bind(cartController));
+router.post("/add", cartController.addToCart.bind(cartController));
+router.post("/remove", cartController.removeFromCart.bind(cartController));
+router.post("/update", cartController.updateCart.bind(cartController));
 
 export default router;
