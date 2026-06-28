782ecaf927c4b4901a42805dafafab3735a0cec6
Omoit699 <lawrenceomoit66@gmail.com>
2026-06-28 14:17:38 +0300

    Update App.jsx

--- ecommerce-website/frontend/src/App.jsx
+++ ecommerce-website/frontend/src/App.jsx
@@ -3,13 +3,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
+import Cart from "./pages/Cart.jsx";
+import Checkout from "./pages/Checkout.jsx";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+
+        {/* 🏠 Home Page */}
         <Route path="/" element={<Home />} />
+
+        {/* 📦 Products Page (optional but useful) */}
         <Route path="/products" element={<Products />} />
+
+        {/* 🛒 Cart Page */}
+        <Route path="/cart" element={<Cart />} />
+
+        {/* 💳 Checkout Page */}
+        <Route path="/checkout" element={<Checkout />} />
+
       </Routes>
     </Router>
   );
